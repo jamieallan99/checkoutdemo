7603bc7e8c2eef0aaacb215197b541536898f977
Jamie Allan <jamiealexanderallan@outlook.com>
2024-05-27 16:10:36 +0100

    Basic Sum

--- checkout/checkout.go
+++ checkout/checkout.go
@@ -1,1 +1,15 @@
 package checkout
+
+var prices = map[string]int {
+	"A": 50,
+	"B": 30,
+	"C": 20,
+}
+
+func SumItems(barcodes []string) int {
+	var sum int
+	for _,b := range barcodes {
+		sum += prices[b]
+	}
+	return sum
+}
