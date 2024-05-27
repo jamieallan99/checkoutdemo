db08260f7c4bb2c4d7eb76e1bf2204f4223f551a
Jamie Allan <jamiealexanderallan@outlook.com>
2024-05-27 16:48:33 +0100

    Change test to expect decimal prices

--- checkout/checkout_test.go
+++ checkout/checkout_test.go
@@ -1,20 +1,24 @@
 package checkout
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/shopspring/decimal"
+)
 
 func TestSumItems(t *testing.T) {
 	testtable := []struct {
 		name     string
 		barcodes []string
-		expected int
+		expected Price
 	}{
-		{"Basic Sum", []string{"A", "B", "C"}, 100},
-		{"Multibuy Sum", []string{"A", "A", "A"}, 130},
+		{"Basic Sum", []string{"A", "B", "C"}, Price{decimal.NewFromInt(100)}},
+		{"Multibuy Sum", []string{"A", "A", "A"}, Price{decimal.NewFromInt(130)}},
 	}
 	for _, tr := range testtable {
 		t.Run(tr.name, func(t *testing.T) {
 			result := SumItems(tr.barcodes)
-			if result != tr.expected {
+			if !tr.expected.Amount.Equal(result.Amount) {
 				t.Errorf("Incorrect Sum expected: %d, got: %d", tr.expected, result)
 			}
 		})
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module checkoutdemo
 
 go 1.22.3
+
+require github.com/shopspring/decimal v1.4.0
