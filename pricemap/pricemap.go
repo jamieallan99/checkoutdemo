491e3b5597f4abd4c478f802d8df561ebe143573
Jamie Allan <jamiealexanderallan@outlook.com>
2024-05-28 13:20:16 +0100

    Add new data structure for storing prices

---
+++ pricemap/pricemap.go
@@ -0,0 +1,16 @@
+package pricemap
+
+import "checkoutdemo/price"
+
+type PriceMap map[string]PriceData
+
+type PriceData struct {
+	Barcode string
+	Price price.Price
+	Multibuy Multibuy
+}
+
+type Multibuy struct {
+	Count int64
+	Price price.Price
+}
