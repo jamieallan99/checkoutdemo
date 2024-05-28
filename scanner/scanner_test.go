bee697051afb2eb2871d70c506328a6861d30639
Jamie Allan <jamiealexanderallan@outlook.com>
2024-05-29 00:09:41 +0100

    Add placeholder tests for initial planned scanner api

---
+++ scanner/scanner_test.go
@@ -0,0 +1,23 @@
+package scanner
+
+import "testing"
+
+func TestFinishTransaction(t *testing.T) {
+	t.Errorf("placeholder")
+}
+
+func TestSaveTransaction(t *testing.T) {
+	t.Errorf("placeholder")
+}
+
+func TestLoadTransaction(t *testing.T) {
+	t.Errorf("placeholder")
+}
+
+func TestStartTransaction(t *testing.T) {
+	t.Errorf("placeholder")
+}
+
+func TestScanItem(t *testing.T) {
+	t.Errorf("placeholder")
+}
